5e87074c30960dbb11b1f892ed0931c0485cc5e8
TheRogueNinja <shreyassawant018@gmail.com>
2019-06-10 08:49:13 +0530

    Added New files

---
+++ src/Newpara.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Checkbox from "./Checkbox"
+ 
+function Newpara(){
+    return(
+        <div>
+            <Checkbox item="Attend Russel Peters Show Live"/>
+            <Checkbox item="Visit 'The Office' set in Scranton"/>
+            <Checkbox item="Visit Iceland once"/>
+            <Checkbox item="Get a Master's Degree in Comp Sci"/>
+            <Checkbox item="Followed by a job in the states"/>
+
+        </div>
+    )
+
+}
+
+export default Newpara;
