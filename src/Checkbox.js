34d54b138deec368dc6d9cad14a5c518300391c8
TheRogueNinja <shreyassawant018@gmail.com>
2019-07-10 22:48:09 +0530

    Added Strike out feature

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 
 function Checkbox(props){
@@ -11,15 +10,6 @@ function Checkbox(props){
         <div className="todo-item">
             <input type = "checkbox" checked = {props.todoItem.completed} onChange={()=>props.handleChange(props.todoItem.id)}/>
             <p style={props.todoItem.completed ? styleProp : null}>{props.todoItem.text}</p>
-=======
-import React from 'react'
-
-function Checkbox(props){
-    return(
-        <div>
-            <input type = "checkbox"/>
-            <p>{props.item}</p>
->>>>>>> 5e87074c30960dbb11b1f892ed0931c0485cc5e8
         </div>
     )
 }
